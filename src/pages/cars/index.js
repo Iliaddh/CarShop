e61a89bc10746b9c5cdb760a281a42d6c66ad9de
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-02-23 20:50:39 -0500

    Cars page component structure created

---
+++ src/pages/cars/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import carsData from '../../../data/carsData';
+import CarsPage from '../../../components/templates/CarsPage';
+
+function index() {
+  return (
+    <div>
+      <CarsPage data={carsData}/>
+    </div>
+  )
+}
+
+export default index;
