bf8d588d1b5b78602fab91ab3e41a69839feeda1
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-02-23 22:05:12 -0500

    Card details pages added to router

--- src/pages/cars/[carId].js
+++ src/pages/cars/[carId].js
@@ -1,11 +1,14 @@
 import { useRouter } from "next/router";
+import carsData from "../../../data/carsData";
+import CarDetails from "../../../components/templates/CarDetails"
 
 function CarDetail() {
     const router = useRouter();
-    const {cardId} = router.query;
-    console.log(cardId)
+    const {carId} = router.query;
+    const carDetails = carsData[carId -1];
+    console.log(carDetails)
   return (
-    <div>CarDetail</div>
+    <CarDetails { ... carDetails} />
   )
 }
 
