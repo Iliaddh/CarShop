8956e22f9844b4be7f657954aa9e70c3e1d92bfa
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-02-23 22:49:29 -0500

    Categories section and its functionality

--- src/pages/cars/index.js
+++ src/pages/cars/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import carsData from '../../../data/carsData';
 import CarsPage from '../../../components/templates/CarsPage';
+import Categories from '../../../components/module/Categories';
 
 function index() {
   return (
     <div>
-      
+      <Categories/>
       <CarsPage data={carsData}/>
     </div>
   )
